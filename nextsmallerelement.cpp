33e51cae3bbd15b028437eb52d901a8e5a0e26a9
MOHAMMAD AZHAR SOFI <98463584+azharsofi@users.noreply.github.com>
2023-03-20 19:41:57 +0530

    Create nextsmallerelement.cpp

---
+++ nextsmallerelement.cpp
@@ -0,0 +1,19 @@
+#include <stack>
+vector<int> nextSmallerElement(vector<int> &arr, int n)
+{
+    // Write your code here.
+    stack<int> s;
+    s.push(-1);
+    vector<int> ans(n);
+    for(int i=n-1;i>=0;i--){
+        int curr=arr[i];
+        while(s.top()>=curr){
+            s.pop();
+        }
+        //stack ka top is answer
+        ans[i]=s.top();
+        s.push(curr);
+    }
+    return ans;
+
+}
